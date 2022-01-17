dfc83a6a0e807612362fc90b6a2758732190c8ca
CARLOS..- <96316594+CaRLos1227@users.noreply.github.com>
2022-01-17 09:25:40 -0300

    update exports.js

--- routes/exports.js
+++ routes/exports.js
@@ -2,6 +2,7 @@
 const { image } = require('./image');
 const { xvideos } = require('./xvideos');
 const { youtube } = require('./youtube');
-const { youtube_download } = require('./youtube-downloads'); 
+const { youtube_download } = require('./youtube-downloads');
+const { ytmp3 } = require('./ytmp3');
 
 module.exports = { image, xvideos, youtube, youtube_download };
