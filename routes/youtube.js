678a3f0574022db1627ab3bb63eb233cc07443b4
CARLOS..- <96316594+CaRLos1227@users.noreply.github.com>
2022-02-26 13:00:54 -0300

    update youtube.js

--- routes/youtube.js
+++ routes/youtube.js
@@ -1,23 +1,20 @@
 const request = require('request');
 
-const formatUrlYTB = (inputUrl) => {
-  var outputUrl;
-  if (inputUrl.startsWith('https://youtube.com/')) {
-    var id = inputUrl.match(/\?v=(.+)/gi)[0].split('?v=')[1];
-    outputUrl = `https://m.youtube.com/watch?v=${id}&feature=youtu.be`;
-  } else if (inputUrl.startsWith('https://youtu.be/')) {
-    var uid = inputUrl.split('/')[inputUrl.split('/').length - 1];
-    outputUrl = `https://m.youtube.com/watch?v=${uid}&feature=youtu.be`;
-  }
-  return outputUrl;
-};
-
-const formatDate = (date) => {
+function formatDate(date) {
   var split = date.split('-');
   return split[split.length - 1] + '/' + split[1] + '/' + split[0];
-};
+}
 
-function youtube(req, res, apikey) {
+function gethtml(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (err, req, body) => {
+      if (err) reject(err);
+      if (body) resolve(body);
+    });
+  });
+}
+
+async function youtube(req, res, apikey) {
   var ApiKey = req.query.apikey;
   var text = req.query.text;
   if (!ApiKey) return res.send({
@@ -32,57 +29,72 @@ function youtube(req, res, apikey) {
     status: false,
     message: 'text is not defined'
   });
-  const search = (word) => {
-    var url = `https://m.youtube.com/results?sp=mAEA&search_query=${word}`;
-
-    console.log(url);
-    request(url, (err, req, body) => {
-      if (err) return console.log(err);
-      var regex = /var ytInitialData = {"responseContext":(.+?)};/gi;
-      var data = JSON.parse(body.match(regex)[0].split('var ytInitialData = ').join('').split(';').join(''));
-
-      var results = [];
-      for (let i of data.contents.twoColumnSearchResultsRenderer.primaryContents.sectionListRenderer.contents[0].itemSectionRenderer.contents) {
-        if (Object.keys(i)[0] == 'videoRenderer') {
-          var info = i.videoRenderer;
-          
-          results.push({
-            title: info.title.runs[0].text,
-            source: 'https://youtu.be/' + info.videoId,
-            views: info.viewCountText.simpleText.split(' views').join(''),
-            thumbnail: info.thumbnail.thumbnails[0].url.split('?')[0],
-            publication: '',
-            duration: info.lengthText.simpleText,
-            channel: [{
-              title: info.ownerText.runs[0].text,
-              source: 'https://youtube.com/channel/' + info.ownerText.runs[0].navigationEndpoint.browseEndpoint.browseId,
-              thumbnail: info.channelThumbnailSupportedRenderers.channelThumbnailWithLinkRenderer.thumbnail.thumbnails[0].url.split('=')[0],
-              description: {
-                text: ''
-              }
-            }]
-          });
+  async function start(query) {
+    var body = await gethtml(`https://www.youtube.com/results?search_query=${encodeURIComponent(query)}`);
+    var infos = {
+      titles: [],
+      videoIds: [],
+      thumbnails: [],
+      channel_thumbnails: [],
+      durations: [],
+      views: []
+    };
+    JSON.parse(body.match(/var ytInitialData = {(.+?)};/gi)[0].replace(/var ytInitialData = |;/gi, '')).contents.twoColumnSearchResultsRenderer.primaryContents.sectionListRenderer.contents[0].itemSectionRenderer.contents.map(i => {
+      if (Object.keys(i)[0] == 'videoRenderer') {
+        if (i.videoRenderer.hasOwnProperty('lengthText')) {
+          infos.videoIds.push(i.videoRenderer.videoId);
+          infos.thumbnails.push(i.videoRenderer.thumbnail.thumbnails[0].url.split('?')[0]);
+          infos.titles.push(i.videoRenderer.title.runs[0].text);
+          infos.durations.push(i.videoRenderer.lengthText.simpleText);
+          infos.views.push(i.videoRenderer.viewCountText.simpleText.replace(/visualizações| |views/gi, ''));
+          infos.channel_thumbnails.push(i.videoRenderer.channelThumbnailSupportedRenderers.channelThumbnailWithLinkRenderer.thumbnail.thumbnails[0].url.split('=')[0]);
+        } else if (!i.videoRenderer.hasOwnProperty('lengthText')) {
+          infos.videoIds.push(i.videoRenderer.videoId);
+          infos.thumbnails.push(i.videoRenderer.thumbnail.thumbnails[0].url.split('?')[0]);
+          infos.titles.push(i.videoRenderer.title.runs[0].text);
+          infos.durations.push('no duration - live');
+          infos.views.push(i.videoRenderer.viewCountText.runs[0].text);
+          infos.channel_thumbnails.push(i.videoRenderer.channelThumbnailSupportedRenderers.channelThumbnailWithLinkRenderer.thumbnail.thumbnails[0].url.split('=')[0]);
         }
       }
-
-      request(formatUrlYTB(results[0].source), (err, req, body) => {
-        if (err) return console.log(err);
-        console.log(formatUrlYTB(results[0].source));
-        var regex = /var ytInitialPlayerResponse = {"responseContext":{"(.+)}}}};/gi;
-        var json = JSON.parse(body.match(regex)[0].split('var ytInitialPlayerResponse = ').join('').split(';').join(''));
-
-        results[0].publication = formatDate(json.microformat.playerMicroformatRenderer.publishDate);
-
-        results[0].channel[0].description.text = json.microformat.playerMicroformatRenderer.description.simpleText;
-
-        res.json({
-          status: true,
-          result: results[0]
-        });
-      });
     });
-  };
-  search(text);
+
+    var body2 = await gethtml(`https://www.youtube.com/watch?v=${infos.videoIds[0]}`);
+    var data = JSON.parse(body2.match(/var ytInitialPlayerResponse = {(.+?)};/gi)[0].replace(/var ytInitialPlayerResponse = |;/gi, ''));
+    var reason;
+
+    if (data.hasOwnProperty('playabilityStatus')) {
+      reason = data.playabilityStatus.status == 'ERROR';
+    }
+    if (reason) return {
+      message: "video indisponível"
+    };
+
+    var object = {
+      videoId: infos.videoIds[0],
+      title: infos.titles[0],
+      source: 'https://youtu.be/' + infos.videoIds[0],
+      thumbnail: infos.thumbnails[0],
+      duration: infos.durations[0],
+      views: infos.views[0],
+      publicationDate: formatDate(data.microformat.playerMicroformatRenderer.publishDate),
+      channel: {
+        title: data.microformat.playerMicroformatRenderer.ownerChannelName,
+        source: 'https://youtube.com/channel/' + data.microformat.playerMicroformatRenderer.externalChannelId,
+        thumbnail: infos.channel_thumbnails[0],
+        description: {
+          text: data.microformat.playerMicroformatRenderer.description.simpleText
+        }
+      },
+      category: data.microformat.playerMicroformatRenderer.category
+    };
+
+    res.json({
+      status: true,
+      result: object
+    });
+  }
+  await start(text);
 }
 
 module.exports = {
